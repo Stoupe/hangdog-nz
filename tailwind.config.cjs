b6dfbe3f9e0a76279b58092cf32e157a594a556a
Henry Stoupe <h.stoupe@gmail.com>
2023-04-16 22:50:18 +1200

    Add homepage animation and second page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         jost: ["Jost", "sans-serif"],
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        fadeIn: "fadeIn 0.4s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
